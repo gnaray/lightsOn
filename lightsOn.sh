19423947d983be1441ab81111438322db7f99b63
Gábor Náray <drszamo@gmail.com>
2025-08-04 15:59:07 +0200

    Made dpmsStatus variable local and calling xset with $() syntax.

--- lightsOn.sh
+++ lightsOn.sh
@@ -268,7 +268,7 @@ delayScreensaver()
 
 
     #Check if DPMS is on. If it is, deactivate and reactivate again. If it is not, do nothing.
-    dpmsStatus=`xset -q | grep -ce 'DPMS is Enabled'`
+    local dpmsStatus=$(xset -q | grep -ce 'DPMS is Enabled')
     if (( $dpmsStatus == 1 )); then
         xset -dpms
         xset dpms
